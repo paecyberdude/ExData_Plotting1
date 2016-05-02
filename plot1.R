609a1d57688e5eb20d0e76418cf4f5218596c273
paecyberdude <pierre.evans@gmail.com>
2016-05-02 04:51:22 -0500

    Added files via upload

--- plot1.R
+++ plot1.R
@@ -18,7 +18,7 @@ datafile <- read.csv("household_power_consumption.txt",header=TRUE,sep=";")
 Start_date <-as.Date("01/02/2007")
 End_date   <-as.Date("02/02/2007")
 df13 <-datafile[,c(1,3)]
-converted_dates  <-as.Date(df13$Date, format="%D/%M/%Y"), df13$Global_active_power,)
+converted_dates  <-as.Date(df13$Date, format="%d/%m/%Y"), df13$Global_active_power,)
 filtered_data <-converted_dates [converted_dates$date>=Start_date & converted_dates$date <=End_date,converted_Dates$Global_active_power,]
 cleaned_and_filtered_data <-na.omit(filtered_data)
 hist(cleaned_and_filtered_data$Global_active_power,main="Global Active Power",col="orange",xlab="Global Active Power (kilowatts)",xlim=c(0,6),ylab="Frequency",ylim=c(0,1200))
